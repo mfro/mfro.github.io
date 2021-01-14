2b6976b437798287dec6d1d50563c6bc5efef33c
Max Froehlich <max@mfro.me>
2021-01-14 11:58:54 -0500

    Add stuff from old site

---
+++ list/precache-manifest.854dd309759a809034825576a4a11c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "973c59eabbbfb947eca7",
+    "url": "css/app.0694a902.css"
+  },
+  {
+    "revision": "973c59eabbbfb947eca7",
+    "url": "js/app.78765a5e.js"
+  },
+  {
+    "revision": "acab1903a382fb9e747c",
+    "url": "css/chunk-vendors.d7686d56.css"
+  },
+  {
+    "revision": "acab1903a382fb9e747c",
+    "url": "js/chunk-vendors.d49b6d34.js"
+  },
+  {
+    "revision": "f6f679a0ccdf828f07e20b8bb165b194",
+    "url": "index.html"
+  }
+];
