8569401525562833c8173e15d3fec806b9874659
Max Froehlich <max@mfro.me>
2021-01-14 17:24:38 -0500

    Revert "gh-pages"
    This reverts commit 7704324cec53148587be401459dc1a1589ab16a3.

---
+++ list/precache-manifest.09e564f20c014f4623c7710b2e9c0ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d680d7892832cc03f03",
+    "url": "css/app.7c423133.css"
+  },
+  {
+    "revision": "9d680d7892832cc03f03",
+    "url": "js/app.7999ef81.js"
+  },
+  {
+    "revision": "acab1903a382fb9e747c",
+    "url": "css/chunk-vendors.d7686d56.css"
+  },
+  {
+    "revision": "acab1903a382fb9e747c",
+    "url": "js/chunk-vendors.d49b6d34.js"
+  },
+  {
+    "revision": "17fe136e39db54bd0db68603120c43ff",
+    "url": "index.html"
+  }
+];
---
+++ list/precache-manifest.418f962336a41b51c641f8c190d417f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43ebea16b74aaccf4a87",
+    "url": "css/app.49a78297.css"
+  },
+  {
+    "revision": "43ebea16b74aaccf4a87",
+    "url": "js/app.bc151582.js"
+  },
+  {
+    "revision": "acab1903a382fb9e747c",
+    "url": "css/chunk-vendors.d7686d56.css"
+  },
+  {
+    "revision": "acab1903a382fb9e747c",
+    "url": "js/chunk-vendors.d49b6d34.js"
+  },
+  {
+    "revision": "23ce49c4a53ef5985d6314b21c5c45e4",
+    "url": "index.html"
+  }
+];
